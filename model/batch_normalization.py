9a7f5dc802cce2ec5d37892e3585204e337c82af
Xintian Yuan <ivy-yxt@hotmail.com>
2026-06-30 20:10:44 -0400

    Update Batch normalization (PyTorch 3/4)

--- model/batch_normalization.py
+++ model/batch_normalization.py
@@ -10,10 +10,10 @@ class Solution:
         # During inference: normalize using running stats (no batch stats needed)
         # Apply affine transform: y = gamma * x_hat + beta
         # Return (y, running_mean, running_var), all rounded to 4 decimals as lists
-        mean = np.mean(x, axis=0)
-        var = np.var(x, axis=0)
         
         if training:
+            mean = np.mean(x, axis=0)
+            var = np.var(x, axis=0)
             x_hat = (x - mean) / np.sqrt(var + eps)
             running_mean = (1 - momentum) * np.array(running_mean) + momentum * mean
             running_var = (1 - momentum) * np.array(running_var) + momentum * var
